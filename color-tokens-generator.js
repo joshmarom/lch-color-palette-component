1fc83fc6e41f5d14c295ecf1f3eb89112c23c8aa
Josh <josh@josh.co.il>
2022-05-23 19:52:04 +0300

    Improve token render code

--- color-tokens-generator.js
+++ color-tokens-generator.js
@@ -18,16 +18,9 @@ const convertColors = (colors) =>{
     return rgbColors
 }
 
-const renderColorTokens = (rgbColors) =>{
-    let style = []
-
-    style.push(`:host {`)
-    Object.entries(rgbColors).forEach(([colorName, color]) => 
-        style.push(`--${colorName}: ${color};`)
-    )
-    style.push(`}`)
-    
-    return style.join(' ')
+const renderColorTokens = (rgbColors) => {
+    const tokens = Object.entries(rgbColors).map(([colorName, color]) => (`--${colorName}: ${color};`))
+    return `:host() {${ tokens.join(' ') }}`
 }
 
 export const generateColorTokens = (colors) => {
